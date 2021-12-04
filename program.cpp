1a59860717adbc3f5e1a12a867a8063419caabd5
Abdillah Ahmad <abdilh_ahmad_official@yahoo.com>
2021-12-04 14:14:30 +0700

    uwu

--- program.cpp
+++ program.cpp
@@ -37,6 +37,7 @@ double f(double x){
     for(i=1;i<=cnt;i++){
         if(fabs(c[i].x-x)>=c[i].r)continue;
         double h= sqrt(c[i].r*c[i].r-(c[i].x-x)*(c[i].x-x));
+        // a[lct].l = titik bawah; a[lct].r = titik atas
         a[++lct].l=c[i].y-h;
         a[lct].r=c[i].y+h;
     }
@@ -44,26 +45,39 @@ double f(double x){
     double len=0,last=-INF;
     sort(a+1,a+lct+1);
     for(i=1;i<=lct;i++){
-        if(a[i].l>last){len+=a[i].r-a[i].l;last=a[i].r;}
-        else if(a[i].r>last){len+=a[i].r-last;last=a[i].r;}
+        if(a[i].l>last){
+            len+=a[i].r-a[i].l;
+            last=a[i].r;
+        }
+        else if(a[i].r>last){
+            len+=a[i].r-last;
+            last=a[i].r;
+        }
     }
+    cout << "f("<<x<<")="<<len;
     return len;
 }
 inline double sim(double l,double r){
     return (f(l)+4*f((l+r)/2)+f(r))*(r-l)/6;
 }
-double solve(double l,double r,double S){
+double solve(double l,double r,double S, double depth){
+    double res;
     double mid=(l+r)/2;
     double ls=sim(l,mid);
     double rs=sim(mid,r);
-    if(fabs(rs+ls-S)<eps)return ls+rs;
-    return solve(l,mid,ls)+solve(mid,r,rs);
+    if(fabs(rs+ls-S)<eps)res = ls+rs;
+    else res = solve(l,mid,ls,depth+1)+solve(mid,r,rs,depth+1);
+    for (int i=1; i<depth; i++) cout << " ";
+    cout << "l " << l << " r " << r;
+    cout << "result " << res << endl;
+    return res;
 }
 int main(){
+    // input
     cout << "Masukkan banyak lingkaran: ";
     n=read();
     int i,j;
-    double L=INF,R=-INF;
+    double L=INF;
     for(i=1;i<=n;i++){
         cout << "Lingkaran ke-" << i << " (x y r): ";
         c[i].x=read();
@@ -77,6 +91,7 @@ int main(){
         cout << "Circle " << i << ": c(" << c[i].x << "," << c[i].y << "); r = " << c[i].r << endl;
     }
 
+    // lingkaran yang berada di dalam lingkaran lain dihapus
     for(i=1;i<n;i++) {
         for(j=i+1;j<=n;j++) {
             if(c[j].r-c[i].r>=dist(c[i],c[j])) {
@@ -99,6 +114,7 @@ int main(){
         cout << "Circle " << i << ": c(" << c[i].x << "," << c[i].y << "); r = " << c[i].r << endl;
     }
 
+    // membuat array b
     double tmp=-INF;
     for(i=1;i<=cnt;i++){
         b[i].l=c[i].x-c[i].r;
@@ -112,12 +128,13 @@ int main(){
     for (i=1;i<=cnt;i++) {
         cout << "Line " << i << ": l = " << b[i].l << ", r = " << b[i].r << endl;
     }
+    // loop utama
     double ans=0, ans_inc=0;
     for(i=1;i<=cnt;i++){
         cout << "start iterate i = " << i << ": (" << b[i].l << " -- " << b[i].r << ") tmp = " << tmp;
         if(b[i].r<=tmp)continue;
         L=max(tmp,b[i].l);
-        ans_inc=solve(L,b[i].r,sim(L,b[i].r));
+        ans_inc=solve(L,b[i].r,sim(L,b[i].r), 1);
         ans += ans_inc;
         cout << " ans_inc = " << ans_inc << " [ans = " << ans << "]" << endl;
         tmp=b[i].r;
